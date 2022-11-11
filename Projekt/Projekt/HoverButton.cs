c92126b056e12ffb5958c1db28c5faa3ec03c46a
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2022-11-11 10:29:26 +0100

    AmongTheFit

---
+++ Projekt/Projekt/HoverButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Projekt
+{
+    class HoverButton : Button
+    {
+        public static readonly DependencyProperty HoverColorProperty = DependencyProperty.Register("HoverColor", typeof(Brush), typeof(HoverButton));
+
+        public Brush HoverColor
+        {
+            get => (Brush)GetValue(HoverColorProperty);
+            set => SetValue(HoverColorProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(HoverButton));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
---
+++ Projekt/Projekt/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class Trainingsplan
+    {
+        string name;
+
+        public Trainingsplan(string _name)
+        {
+            name = _name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Trainingsplan> trainingsplans = new List<Trainingsplan>();
+
+        SoundPlayer soundPlayer = new SoundPlayer("C:\\Users\\Christopher\\OneDrive\\Desktop\\Hear Me Tonight.wav");
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState |= WindowState.Maximized;
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            soundPlayer.Play();
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            soundPlayer.Stop();
+        }
+
+        private void HoverButton_Click(object sender, RoutedEventArgs e)
+        {
+            Trainingsplan plan = new Trainingsplan("Neuer Plan " + (trainingsplans.Count + 1));
+            trainingsplans.Add(plan);   
+
+            TrainingsplanListView.Items.Add(plan.Name);
+        }
+    }
+}
